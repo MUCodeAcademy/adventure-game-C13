15c8ef0644afb67ad3135c628a348370c13bd306
babias <ababias01@gmail.com>
2024-02-12 09:43:11 -0500

    css adjustments

--- src/components/GameOverPopup.js
+++ src/components/GameOverPopup.js
@@ -6,6 +6,7 @@ const GameOverPopup = ({ onRestart }) => {
   return (
     <div className="game-over-popup">
       <h2>GAME OVER</h2>
+      <h4>"Wisdom is chasing you, but you are faster!"</h4>
       <button onClick={onRestart}>Restart</button>
     </div>
   );
--- src/components/TextAdventureGame.js
+++ src/components/TextAdventureGame.js
@@ -1,29 +1,40 @@
 // TextAdventureGame.js
 import React, { useState, useEffect } from 'react';
+import './Inventory.css';
 import Scene from './Scene';
 import GameOver from './GameOver';
 import './Health.css';
+import Inventory from './Inventory';
 
 const TextAdventureGame = () => {
+  const [inventory, setInventory] = useState([]);
   const [currentSceneIndex, setCurrentSceneIndex] = useState(0);
   const [health, setHealth] = useState(50);
   const [gameOver, setGameOver] = useState(false);
+  const addToInventory = (item) => {
+    setInventory((prevInventory) => [...prevInventory, item]);
+  };
 
   const handleChoice = (choiceIndex, damage) => {
     if (gameOver) return;
-
+  
     const choice = scenes[currentSceneIndex].choices[choiceIndex];
     const nextSceneIndex = choice.nextSceneIndex;
-
+  
+    // Check if the choice has an item and add it to the inventory
+    if (choice.item) {
+      addToInventory(choice.item);
+    }
+  
     if (choice.damage) {
       const newHealth = Math.max(0, health - choice.damage);
       setHealth(newHealth);
-
+  
       if (newHealth === 0) {
         setGameOver(true);
       }
     }
-
+  
     setCurrentSceneIndex(nextSceneIndex);
   };
 
@@ -36,26 +47,31 @@ const TextAdventureGame = () => {
   const currentScene = scenes[currentSceneIndex];
 
   return (
-    <div className="scene-container">
-      {gameOver && <GameOver onRestart={restartGame} />}
-
-      <h1>Room {Math.floor(currentSceneIndex / 5) + 1}</h1>
-      <div className="health-container">
-        <h2 className={`health ${health <= 30 ? 'low-health' : ''}`}>Health: {health}</h2>
+    <div className="game-container">
+      <div className="inventory-container">
+        <Inventory items={inventory} />
       </div>
-      <Scene scene={currentScene} onChoice={handleChoice} />
+      <div className="scene-container">
+        {gameOver && <GameOver onRestart={restartGame} />}
+        <h3>Room {Math.floor(currentSceneIndex / 4) + 1}</h3>
+        <div className="health-container">
+          <h2 className={`health ${health <= 30 ? 'low-health' : ''}`}>Health: {health}</h2>
+        </div>
+        <Scene scene={currentScene} onChoice={handleChoice} />
+      </div>
+
     </div>
   );
 };
 
 const scenes = [
   {
-    description: 'Scene 1 in Room 1 MAIN <br> "You stand in a peculiar room... (your starting scene)',
+    description: 'Scene 1 in Room 1 MAIN\nYou stand in a peculiar room... (your starting scene)',
     image: '/images/mainroom.jpg',
     choices: [
-      { text: 'Choice 1 - "Examine the contents of the desk drawer."', nextSceneIndex: 1 },
-      { text: 'Choice 2 - "Carefully look under the couch for any hidden items or clues" ', nextSceneIndex: 3 },
-      { text: 'Choice 3 - "Take a nap, wait for a miracle. DAMAGE"', nextSceneIndex: 2, damage: 10 }, // deducts 10 health
+      { text: 'Choice 1 - "Examine the contents of the desk drawer."', nextSceneIndex: 1},
+      { text: 'Choice 2 - "Carefully look under the couch for any hidden items or clues"', nextSceneIndex: 3 },
+      { text: 'Choice 3 - "Take a nap, wait for a miracle. DAMAGE"', nextSceneIndex: 2, damage: 20 },
     ],
   },
   {
@@ -63,7 +79,7 @@ const scenes = [
     image: '/images/watch.jpg',
     choices: [
       { text: 'GO to Main Room', nextSceneIndex: 0 },
-      
+            
     ],
   },
   {
@@ -71,7 +87,7 @@ const scenes = [
     image: '/images/skeleton.jpg',
     choices: [
       { text: 'GO to Main Room', nextSceneIndex: 0 },
-      
+            
     ],
   },
   {
@@ -79,9 +95,9 @@ const scenes = [
     image: '/images/key.jpg',
     choices: [
       { text: 'GO to Main Room', nextSceneIndex: 0 },
+      { text: 'Take the KEY',nextSceneIndex: 2, item: 'Key'},
     ],
   },
 ];
-
 
 export default TextAdventureGame;
